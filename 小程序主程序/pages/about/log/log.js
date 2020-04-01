c4e42ebf5100ac7f3c8d4c344e594db38d3cebb2
HiramWong <1275654934@qq.com>
2020-04-01 14:30:52 +0800

    first submit

---
+++ 小程序主程序/pages/about/log/log.js
@@ -0,0 +1,20 @@
+const app = getApp();
+Page({
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    ColorList: app.globalData.ColorList,    
+  },
+  onLoad: function () { },
+  pageBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+});
