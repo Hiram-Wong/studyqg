8491ce6244684f1bbc5b88d306fa55786ebab294
HiramWong <1275654934@qq.com>
2020-04-18 16:43:18 +0800

    UpData

--- README.md
+++ README.md
@@ -1,5 +1,50 @@
-# 学习强国助手(微信小程序+后端api)
+# 每日强国『学习强国助手』◕‿-｡
+
+# 吉祥物护法『来源于网络，如侵权联系删除』 ｡◕‿◕｡
+
+```python
+　　  へ　　　　／|
+　　/＼7　　   / ／
+　 /　│　　 ／　／
+　│　Z ＿,＜　／　　 /`ヽ
+　│　　　　　ヽ　　 /　　〉
+　 Y　　　　　`　 /　　/
+　ｲ●　､　●　　⊂⊃〈　　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿_|
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    皮卡丘护法  永无BUG
+
+```
+
 # 项目依赖
-* 基于微信小程序 + flask框架（python）+ colorui框架
+
+1. 小程序『含微信小程序 && QQ小程序』，组件采用[colorui组件库](https://github.com/weilanwl/ColorUI)
+2. 后端 开发了Flask和Django两个版本『均基于Python』
+3. 题库来源于[XXQG-TiaoZhanDaTi-TiKu](https://github.com/JiangZY5959/XXQG-TiaoZhanDaTi-TiKu/blob/master/README.md) ✌ 目前数据库数据整理至2020年3月23日 共1416
+
+* Flask版本
+
+1. P.s: Flask只开发了api接口,无后台
+2. 依赖第三方库『flask，pymysql，flask_cors』
+
+* Django版本
+
+1. P.s: Django开发了api接口和后台管理『采用[simpleui主题](https://github.com/newpanjing/simpleui)』,支持Banner·题库·开发日志的修改
+2. 依赖第三方库『django，django-cors-headers,django-simpleui』
+3. 后台预览
+![Home](https://ae01.alicdn.com/kf/Hdf5ae107b55b4e08b4d275d74edabd1cF.png)
+![Banner](https://ae01.alicdn.com/kf/Hd69b9332bb6842baa401d1d9fa259060j.png)
+![QuestionLib](https://ae01.alicdn.com/kf/H7dec88f2857546579fd3d67ac9785258r.png)
+![QuestionLib-edit](https://ae01.alicdn.com/kf/H180d8cb4814a48b787ccd0e805d83785Z.png)
+![Log](https://ae01.alicdn.com/kf/H7780b99381d64d358dd4e11f78d32b53e.png)
+
 # 预览
+
 ![微信小程序](https://ae01.alicdn.com/kf/H3824bf27301e460c9f97e0fc11069d25Z.png)
+![QQ小程序](https://ae01.alicdn.com/kf/Hd455a251f2e24ec6bedf82443f6896fcf.png)
---
+++ 后端/Flask版本/xuexi-api.py
@@ -0,0 +1,141 @@
+# -*- encoding: utf-8 -*-
+
+from flask import Flask,request,Response
+import pymysql,json
+from flask_cors import *
+
+app = Flask(__name__)
+# 解决跨域问题
+CORS(app, supports_credentials=True)
+
+def mysql():
+    conn = pymysql.connect(
+        host='localhost',
+        user='study',
+        passwd='study',
+        db='study',
+        charset='utf8'
+    )
+    return conn
+
+def search(key):
+    mysqls = mysql()
+    cur = mysqls.cursor()
+    sql_search = 'SELECT * FROM study WHERE question LIKE "%{}%"'.format(key)
+    sql_total = 'SELECT COUNT(*) FROM study'
+    res = cur.execute(sql_search)
+    data_search = cur.fetchall()
+    res = cur.execute(sql_total)
+    data_total = cur.fetchone()
+    search_all_list = []
+    for i in range(len(data_search)):
+        search_list = {}
+        search_list['id'] = data_search[i][0]
+        search_list['question'] = data_search[i][1]
+        search_list['answer'] = data_search[i][2]
+        search_list['A'] = data_search[i][3]
+        search_list['B'] = data_search[i][4]
+        search_list['C'] = data_search[i][5]
+        search_list['D'] = data_search[i][6]
+        search_list['answer_detail'] = data_search[i][2]+":"+search_list[data_search[i][2]]
+        search_all_list.append(search_list)
+    res = {
+        'code': 200,
+        'msg': 'success',
+        'total' : data_total[0],
+        'data': search_all_list
+        }
+    print(res)
+    return Response(json.dumps(res, ensure_ascii=False), mimetype='application/json')
+
+def detail(id):
+    mysqls = mysql()
+    cur = mysqls.cursor()
+    sql = 'SELECT * FROM study WHERE id = {}'.format(id)
+    cur.execute(sql)
+    data = cur.fetchone()
+    print(data)
+    arrlist = {}
+    for i in data:
+        arrlist['question'] = data[1]
+        arrlist['answer'] = data[2]
+        arrlist['A'] = data[3]
+        arrlist['B'] = data[4]
+        arrlist['C'] = data[5]
+        arrlist['D'] = data[6]
+    res = {
+        'code': 200,
+        'msg': 'success',
+        'data': arrlist
+    }
+    print(res)
+    return Response(json.dumps(res, ensure_ascii=False), mimetype='application/json')
+
+@app.route('/', methods=['GET'])
+def index():
+    key = request.args.get('key')
+    res = search(key)
+    return res
+
+@app.route('/banner', methods=['GET'])
+def banner():
+    res = [{
+        'id': 0,
+        'type': 'image',
+        'url': 'https://ae01.alicdn.com/kf/H4d220eff215643c8a99c02fc5c790624s.png'
+    },{
+        'id': 1,
+        'type': 'image',
+        'url': 'https://ae01.alicdn.com/kf/H9ca42dcbfd2e4631997c4d137be23450e.png'
+    },{
+        'id': 2,
+        'type': 'image',
+        'url': 'https://ae01.alicdn.com/kf/H8243d5fcd06f4c9ebe2b52b982f65762a.png'
+    }],
+    return Response(json.dumps(res, ensure_ascii=False), mimetype='application/json')
+
+@app.route('/id', methods=['GET'])
+def id():
+    id = request.args.get('id')
+    res = detail(id)
+    return res
+
+@app.route('/log', methods=['GET'])
+def log():
+    res = [{
+      'versation': '1.0.0',
+      'time': '2020/03/31',
+      'description': '完成基础功能的开发'
+    }, {
+      'versation': '1.0.0',
+      'time': '2020/04/01',
+      'description': '微信小程序1.0.0上线'
+    }, {
+      'versation': '1.1.0',
+      'time': '2020/04/02',
+      'description': '1.完成语音识别功能\n2.完成详情页的开发\n3.加入客服解答'
+    }, {
+      'versation': '1.1.0',
+      'time': '2020/04/03',
+      'description': '完成1.1.0的审核，微信小程序端线上全量推送'
+    }, {
+      'versation': '1.2.0',
+      'time': '2020/04/03',
+      'description': '给适当的页面增加回顶部'
+    }, {
+      'versation': '1.2.0',
+      'time': '2020/04/5',
+      'description': '完成1.2.0的审核，微信小程序端线上全量推送'
+    }, {
+      'versation': '1.2.1',
+      'time': '2020/04/11',
+      'description': '1.修复部分资源失效\n2.完成QQ小程序1.2.1的兼容[去除语音识别和客服功能]'
+    }, {
+      'versation': '1.2.1',
+      'time': '2020/04/13',
+      'description': '1.完成1.2.1的审核，微信小程序端线上全量推送\n2.QQ小程序1.2.1上线'
+    }]
+    return Response(json.dumps(res, ensure_ascii=False), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', '2333', debug=False)
